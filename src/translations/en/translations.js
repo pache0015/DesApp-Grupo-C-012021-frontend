84a11a5ac393b66b683ba4c8b6fac90791b9fa4d
pache0015 <pache.facundo@gmail.com>
2021-06-08 11:48:10 -0300

    Added i18n

---
+++ src/translations/en/translations.js
@@ -0,0 +1,9 @@
+export const TRANSLATIONS_EN = {
+    welcome:"Welcome to the tutorial",
+    login:"Login",
+    or:"or",
+    signUp:"Sign up",
+    name:"Name",
+    email:"Email",
+    password:"Password",
+};
