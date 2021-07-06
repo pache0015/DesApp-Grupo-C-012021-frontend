8441eb875914f3a5dab530300cc4ccc3cdeedc1f
pache0015 <pache.facundo@gmail.com>
2021-07-06 16:33:46 -0300

    view token and metrics

--- src/pages/userHome/UserHome.js
+++ src/pages/userHome/UserHome.js
@@ -1,22 +1,48 @@
-import React, {useEffect, useState} from "react";
-import getUserData from "../../services/getUserData";
+import React, {useContext, useEffect, useState} from "react";
+import getUserDataService from "../../services/getUserDataService";
+import Header from "../../components/header/Header";
+import './userHome.css';
+import UserContext from "../../contexts/UserContext";
 
-export default function UserHome({apiKey}){
+export default function UserHome(){
 
     const [loading, setLoading] = useState(false);
-    const [data, setData] = useState([]);
+    const [userData, setUserData] = useState([]);
+    const [titlesReviewed, setTitlesReviewed] = useState()
+    const [reviewsWritten, setReviewsWritten] = useState()
+    const {token} = useContext(UserContext);
+
 
     useEffect( function () {
         setLoading(true);
-        getUserData({ apiKey })
+        getUserDataService( token )
             .then(data => {
-                setData(data)
+                setUserData(data)
+                console.log(data)
+                setTitlesReviewed(data.titlesReviewed.length)
+                setReviewsWritten(data.reviewsWritten.length)
                 setLoading(false)
             })
-    }, [apiKey]);
+
+    }, [token]);
 
     if (loading) return <h1> Loading </h1>
     return(
-        <h1>HOLA</h1>
+        <div className="home">
+            <Header className="header" />
+            <div className="api-key">
+                <h1>Tu api key es:</h1>
+                <input type="text" value={"Bearer ".concat(token)}/>
+            </div>
+
+            <div className="metricas">
+                <h1>Títulos revisado:</h1>
+                <span>{titlesReviewed}</span>
+            </div>
+            <div className="metricas">
+                <h1>Reseñas escritas:</h1>
+                <span>{reviewsWritten}</span>
+            </div>
+        </div>
     );
 };
