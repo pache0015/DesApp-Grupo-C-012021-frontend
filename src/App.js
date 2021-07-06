94412d633e9f2a07e1772121589cc20e86cfcd02
pache0015 <pache.facundo@gmail.com>
2021-07-06 16:32:28 -0300

    Added Context

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import SwitcherLanguage from "./components/switcherLanguage/switcherLanguage";
 import FormContainer from "./pages/formContainer/formContainer";
 import React from "react";
 import {Fragment} from "react";
-import { Link, Route} from "wouter"
+import { Route} from "wouter"
 import UserHome from "./pages/userHome/UserHome";
+import Context from "./contexts/UserContext";
+import {UserContextProvider} from "./contexts/UserContext";
 
 function App() {
   return (
-      <Fragment>
+      <UserContextProvider>
           <SwitcherLanguage className="switcher"/>
           <Route path="/" component={ FormContainer } />
+          <Route path="/login" component={ FormContainer } />
           <Route path="/home" component={ UserHome} />
-      </Fragment>
+      </UserContextProvider>
   );
 }
 export default App;
